eaa86380b96cd2ea42af30bb0cd85e7ba0ba2057
GorkaTsybin <egortsybin@yandex.ru>
2024-08-10 19:02:21 +0400

    Add files via upload

---
+++ module_1_5.ru.py
@@ -0,0 +1,9 @@
+imutable_var = ("Zenit", 1, 9, 2, 5)
+print(type(imutable_var))
+print(imutable_var)
+#imutable_var[0] = 1
+# кортеж не изменяемый
+mutable_list = ['Sochi', 'CS', 1, 23]
+print(type(mutable_list))
+mutable_list[0] = 'Moscow'
+print(mutable_list)
